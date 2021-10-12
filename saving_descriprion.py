8914250cfbd02441c43e050c857b28dfd51ea9d6
Daniil-Solo <daniil.solo1723@gmail.com>
2021-10-12 17:37:28 +0300

    correct saving with english words

--- saving_descriprion.py
+++ saving_descriprion.py
@@ -3,9 +3,9 @@ import re
 
 
 def save_description(text):
-    only_letters_and_digits_text = ' '.join(re.findall("[ЁёА-Яа-я\d#]+", text))
+    only_letters_and_digits_text = ' '.join(re.findall("[A-Za-zЁёА-Яа-я\d#]+", text))
     only_lower_register_text = only_letters_and_digits_text.lower()
     row = [only_lower_register_text, "Заполнить"]
-    with open('data.csv', 'a', newline='') as csv_file:
+    with open('.ipynb_checkpoints/data.csv', 'a', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(row)
